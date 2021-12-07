8fde0a75004002a08a81299f423bd165183ee2e2
ldejesuscom <93086938+ldejesuscom@users.noreply.github.com>
2021-12-07 10:06:57 -0500

    Add files via upload

---
+++ new_dev_transaction.py
@@ -0,0 +1,11 @@
+# import Web3 library
+from web3 import Web3
+# Declare Infura Arbitrum MainNet API URL and set HTTP JSON RPC provider
+infura_url = "https://arbitrum-mainnet.infura.io/v3/53298ef4473f41dab399d84ff234f0e7"
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+print("ArbiNyan Deployer Account:")
+print(web3.eth.get_transaction_count("0x8602EE2f8AaEb671E409b26d48E36DD8Cc3B7ED7"))
+
+print("\n\nArbiNyan Whale Account:")
+print(web3.eth.get_transaction_count("0x068f943adbae44c24e5A184010b4733F944b7f31"))
