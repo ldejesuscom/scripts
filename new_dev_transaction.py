ea9d8e56628bc1256d8e04ccd5be879aecb7ff99
ldejesuscom <93086938+ldejesuscom@users.noreply.github.com>
2021-12-07 12:07:45 -0500

    Update new_dev_transaction.py

--- new_dev_transaction.py
+++ new_dev_transaction.py
@@ -7,5 +7,8 @@ web3 = Web3(Web3.HTTPProvider(infura_url))
 print("ArbiNyan Deployer Account:")
 print(web3.eth.get_transaction_count("0x8602EE2f8AaEb671E409b26d48E36DD8Cc3B7ED7"))
 
-print("\n\nArbiNyan Whale Account:")
+print("\n\nArbiNyan Whale Account 1:")
 print(web3.eth.get_transaction_count("0x068f943adbae44c24e5A184010b4733F944b7f31"))
+
+print("\n\nArbiNyan Whale Account 2:")
+print(web3.eth.get_transaction_count("0x930e9b64896431A24c84B3F95B91af3530Ae187F"))
